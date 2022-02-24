990f75e0a92dc41de32a3481c044990db35bef72
Yohan Silva <yohanps.dev@gmail.com>
2022-02-24 19:20:34 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,11 @@ public class Program {
 		for( Seller obj : list) {
 			System.out.println(obj);
 		}
+	
+		System.out.println();		
+		System.out.println("=== TESTE 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, departement);
+		sellerDao.insert(newSeller);
+		System.out.println("Insterted! new id = "+ newSeller.getId());
 	}
 }
